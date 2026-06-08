79311ac116a6aa025f8afc01e287c76796b77848
Anushka251 <anushkam251@gmail.com>
2026-06-08 22:13:40 +0530

    Set test payment amount to 1

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -54,7 +54,7 @@ export default function CheckoutPage() {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          amount: total,
+          amount: 1,
         }),
       });
 
