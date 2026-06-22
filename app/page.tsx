fe61595ba1f9e0067e5e5c2e9732cbbafdd7b320
Anushka251 <54027934+Anushka251@users.noreply.github.com>
2026-06-21 23:14:35 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import Footer from "../components/Footer";
+import Footer from "app/components/Footer";
 
 export default function Home() {
   return (
