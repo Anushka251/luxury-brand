178a0ede0d92242a657a7716dfe69ebba28f3fb1
Anushka251 <54027934+Anushka251@users.noreply.github.com>
2026-06-22 14:41:12 +0530

    Update page.tsx

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -40,7 +40,7 @@ export default function Home(): React.JSX.Element {
           >
             <h1
               className="
-                text-ivory
+                text-[#AF9685]
                 text-5xl
                 sm:text-7xl
                 md:text-8xl
