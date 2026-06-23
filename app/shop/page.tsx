e251d271e04becabe46ec94b34ce5e94cde81db9
Anushka251 <54027934+Anushka251@users.noreply.github.com>
2026-06-23 10:23:29 +0530

    Update page.tsx

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -4,15 +4,14 @@ import Image from "next/image";
 
 export default function Shop() {
   return (
-    <main className="px-6 pt-10 pb-20 max-w-7xl mx-auto">
-      <section className="flex justify-center items-center">
+    <main className="px-6 pt-4 pb-20 max-w-7xl mx-auto">
+      <section className="flex justify-center items-start">
         {products.map((p) => (
           <Link
             key={p.id}
             href={`/product/${p.id}`}
-            className="group w-full max-w-md"
+            className="group w-full max-w-sm"
           >
-            {/* PRODUCT IMAGE */}
             <div className="relative aspect-[3/4] overflow-hidden rounded-lg">
               <Image
                 src={p.coverImage}
@@ -28,7 +27,6 @@ export default function Shop() {
               />
             </div>
 
-            {/* PRODUCT DETAILS */}
             <div className="pt-6 text-center">
               <h2
                 className="
