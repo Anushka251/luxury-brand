03fdba59c26bbdebd76d7ae73c0aef6fe387fae6
Anushka251 <54027934+Anushka251@users.noreply.github.com>
2026-06-24 16:44:28 +0530

    Update page.tsx

--- app/gallery/[index]/page.tsx
+++ app/gallery/[index]/page.tsx
@@ -1,11 +1,9 @@
-"use client";
-
 import GalleryClient from "./GalleryClient";
 
 const images = [
   "/products/crimson-rose/1.jpg",
   "/products/crimson-rose/2.jpg",
-  "/products/crimson-rose/3-v2.jpg",
+  "/products/crimson-rose/3.jpg",
   "/products/crimson-rose/4.jpg",
   "/products/crimson-rose/5.jpg",
   "/products/crimson-rose/6.jpg",
@@ -13,7 +11,7 @@ const images = [
   "/products/crimson-rose/8.jpg",
 ];
 
-export default async function GalleryPage({
+export default async function Page({
   params,
 }: {
   params: Promise<{ index: string }>;
