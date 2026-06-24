ebb8a68b37e4efc820ed8cbb06948792cdd62ac7
Anushka251 <54027934+Anushka251@users.noreply.github.com>
2026-06-24 16:36:59 +0530

    Update page.tsx

--- app/gallery/[index]/page.tsx
+++ app/gallery/[index]/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import Link from "next/link";
-import { useState } from "react";
+import GalleryClient from "./GalleryClient";
 
 const images = [
   "/products/crimson-rose/1.jpg",
@@ -14,134 +13,17 @@ const images = [
   "/products/crimson-rose/8.jpg",
 ];
 
-export default function GalleryPage({
+export default async function GalleryPage({
   params,
 }: {
-  params: { index: string };
+  params: Promise<{ index: string }>;
 }) {
-  const initialIndex = Math.min(
-    Math.max(Number(params.index) || 0, 0),
-    images.length - 1
-  );
-
-  const [index, setIndex] =
-    useState(initialIndex);
-
-  const prev = () => {
-    setIndex((i) =>
-      i === 0
-        ? images.length - 1
-        : i - 1
-    );
-  };
-
-  const next = () => {
-    setIndex((i) =>
-      i === images.length - 1
-        ? 0
-        : i + 1
-    );
-  };
+  const { index } = await params;
 
   return (
-    <main className="fixed inset-0 z-[999] bg-ivory">
-      {/* CLOSE */}
-      <Link
-        href="/product/crimson-rose"
-        className="
-          absolute
-          top-6
-          right-8
-          z-20
-          text-4xl
-          font-light
-          text-gray-500
-          hover:text-black
-          transition
-        "
-      >
-        ×
-      </Link>
-
-      {/* LEFT ARROW */}
-      <button
-        onClick={prev}
-        className="
-          absolute
-          left-6
-          md:left-10
-          top-1/2
-          -translate-y-1/2
-          z-20
-          text-6xl
-          font-light
-          text-gray-500
-          hover:text-black
-          transition
-        "
-      >
-        ‹
-      </button>
-
-      {/* IMAGE */}
-      <div className="h-screen flex items-center justify-center px-20">
-        <img
-          src={images[index]}
-          alt={`Image ${index + 1}`}
-          className="
-            max-w-[90vw]
-            max-h-[90vh]
-            object-contain
-            select-none
-          "
-        />
-      </div>
-
-      {/* RIGHT ARROW */}
-      <button
-        onClick={next}
-        className="
-          absolute
-          right-6
-          md:right-10
-          top-1/2
-          -translate-y-1/2
-          z-20
-          text-6xl
-          font-light
-          text-gray-500
-          hover:text-black
-          transition
-        "
-      >
-        ›
-      </button>
-
-      {/* COUNTER */}
-      <div
-        className="
-          absolute
-          bottom-8
-          left-1/2
-          -translate-x-1/2
-        "
-      >
-        <p
-          className="
-            text-sm
-            text-gray-500
-            tracking-[0.2em]
-            font-light
-            select-none
-          "
-          style={{
-            fontFamily:
-              '"Cormorant Garamond", serif',
-          }}
-        >
-          {index + 1} / {images.length}
-        </p>
-      </div>
-    </main>
+    <GalleryClient
+      images={images}
+      initialIndex={Number(index)}
+    />
   );
 }
