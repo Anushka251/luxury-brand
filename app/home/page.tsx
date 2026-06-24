b657eba265eeeae413a6008adfd3635bda754b61
Anushka251 <54027934+Anushka251@users.noreply.github.com>
2026-06-24 12:39:07 +0530

    Update page.tsx

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -18,7 +18,7 @@ export default function Home(): React.JSX.Element {
             transition={{ duration: 1.5 }}
           >
             <Image
-              src="/avenor-hero-logo.jpg"
+              src="/avenor-logo.jpg"
               alt="AVENOR"
               width={900}
               height={1200}
