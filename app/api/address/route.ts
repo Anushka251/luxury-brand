076f03fddddbff2b400af29c1301d58c2c25743f
Anushka251 <54027934+Anushka251@users.noreply.github.com>
2026-06-26 10:48:56 +0530

    Update route.ts

--- app/api/address/route.ts
+++ app/api/address/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import connectDB from "@/lib/mongodb";
+import { connectDB } from "@/lib/mongodb";
 import Address from "@/models/Address";
 
 // GET
