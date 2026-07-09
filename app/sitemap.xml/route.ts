06b056b2c5bb2d637840448b215f8188acf8d3d1
Anushka251 <54027934+Anushka251@users.noreply.github.com>
2026-07-09 11:33:05 +0530

    Update route.ts

--- app/sitemap.xml/route.ts
+++ app/sitemap.xml/route.ts
@@ -3,21 +3,26 @@ export async function GET() {
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>https://avenorcollection.com/</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
   </url>
   <url>
     <loc>https://avenorcollection.com/shop</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
   </url>
   <url>
     <loc>https://avenorcollection.com/reserve/crimson-rose</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
   </url>
   <url>
     <loc>https://avenorcollection.com/reserve/ivory-blush</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
   </url>
 </urlset>`;
 
   return new Response(xml, {
     headers: {
-      "Content-Type": "application/xml",
+      "Content-Type": "application/xml; charset=utf-8",
+      "Cache-Control": "public, max-age=3600, s-maxage=3600",
     },
   });
 }
