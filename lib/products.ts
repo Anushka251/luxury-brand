d081d48d231c83b1b97216ccb26ba83aab2243b3
Anushka251 <54027934+Anushka251@users.noreply.github.com>
2026-08-20 23:44:48 +0530

    Update products.ts

--- lib/products.ts
+++ lib/products.ts
@@ -88,7 +88,7 @@ Every detail is thoughtfully finished to celebrate the artistry of fine craftsma
     coverImage: "/products/blue-crystal/cover.jpg",
 
     images: [
-      "/products/blue-crystal/1.jpg",
+      "/products/blue-crystal/1.JPG",
       "/products/blue-crystal/2.JPG",
       "/products/blue-crystal/3.JPG",
     ],
