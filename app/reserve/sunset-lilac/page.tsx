f16aa4865efdc30d3aae681cd3823daee876a00e
Anushka251 <54027934+Anushka251@users.noreply.github.com>
2026-08-31 15:55:46 +0530

    Replace product.description with product.detailDescription

--- app/reserve/sunset-lilac/page.tsx
+++ app/reserve/sunset-lilac/page.tsx
@@ -609,7 +609,7 @@ export default function SunsetLilacReservationPage() {
               text-[#6B625B]
             "
           >
-            {product.description}
+            {product.detailDescription}
           </p>
 
 
