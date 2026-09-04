476aaaa55dfc69156bf2d7d3f616ef67764226bf
Anushka251 <54027934+Anushka251@users.noreply.github.com>
2026-09-04 21:16:18 +0530

    Update route.ts

--- app/api/reserve/route.ts
+++ app/api/reserve/route.ts
@@ -145,7 +145,7 @@ export async function POST(req: Request) {
      * =========================================================
      */
 
-    const reservationFee = 2000;
+    const reservationFee = 500;
 
     /*
      * =========================================================
