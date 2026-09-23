775405bbc56ad65a6afa82b6038d31e91572d4be
Anushka251 <54027934+Anushka251@users.noreply.github.com>
2026-09-23 12:42:43 +0530

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,38 +5,26 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: "https://avenorcollection.com/",
       lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 1,
     },
     {
       url: "https://avenorcollection.com/shop",
       lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.9,
     },
     {
       url: "https://avenorcollection.com/reserve/crimson-rose",
       lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.8,
     },
     {
       url: "https://avenorcollection.com/reserve/blue-crystal",
       lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.8,
     },
     {
       url: "https://avenorcollection.com/reserve/ivory-blush",
       lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.8,
     },
     {
       url: "https://avenorcollection.com/reserve/sunset-lilac",
       lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.8,
     },
   ];
 }
