f70b7140360d5a70b6ea392ec5185cce280358d5
Anushka251 <54027934+Anushka251@users.noreply.github.com>
2026-09-23 14:54:37 +0530

    Update page.tsx

--- app/shop/[id]/page.tsx
+++ app/shop/[id]/page.tsx
@@ -3,16 +3,16 @@ import { notFound } from "next/navigation";
 import ProductClient from "@/app/product-components/ProductClient";
 
 interface Props {
-  params: { id: string }; // must be string
+  params: Promise<{ id: string }>;
 }
 
-export default function ShopProductPage({ params }: Props) {
-  const { id } = params; // get the id from the route
+export default async function ShopProductPage({ params }: Props) {
+  const { id } = await params;
 
   // Find the product by ID
   const product = products.find((p) => p.id === id);
 
-  if (!product) return notFound(); // show 404 if not found
+  if (!product) return notFound();
 
   return <ProductClient product={product} />;
 }
